98b0ece900a99fc7aa90824b09f45ee119328fa6
Itzhak Kakon <izakkakon@gmail.com>
2025-05-22 12:37:20 +0300

    add file Message

---
+++ IdfOperation/Intelligence/message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdfOperation.Forces;
+namespace IdfOperation.Intelligence
+{
+    internal class Message
+    {
+        ITerrorist terrorist;
+        string zone;
+        DateTime date;
+        public Message(ITerrorist terrorist, string zone, DateTime date)
+        {
+            this.terrorist = terrorist;
+            this.zone = zone;
+            this.date = date;
+        } 
+    }
+
+}
