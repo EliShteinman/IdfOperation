cf20797eda975aca5d9ac81314311bfb40d4fb72
eli_shteinman <a0533057932@gmail.com>
2025-05-28 10:26:39 +0300

    Add documentation for methods in StrikeAssignmentAnalyzer
    Added XML documentation to improve code clarity and maintainability. This includes summaries, parameter descriptions, and return values for all public methods in the class.

--- IdfOperation/Idf/Commands/AttackCommand/StrikeAssignmentAnalyzer.cs
+++ IdfOperation/Idf/Commands/AttackCommand/StrikeAssignmentAnalyzer.cs
@@ -2,8 +2,16 @@ using IdfOperation.Idf.Constants;
 
 namespace IdfOperation.Idf.Commands.AttackCommand;
 
+/// <summary>
+/// מחלקה סטטית האחראית על ניתוח והקצאת יחידות תקיפה למשימות
+/// </summary>
 public static class StrikeAssignmentAnalyzer
 {
+    /// <summary>
+    /// מחזיר את כל יחידות התקיפה המסוגלות לתקוף באזור מסוים
+    /// </summary>
+    /// <param name="zone">אזור המטרה</param>
+    /// <returns>רשימת יחידות התקיפה המתאימות</returns>
     public static List<StrikeUnit> GetUnitsCapableOfTargeting(string zone)
     {
         var idfInstance = Idf.Instance;
@@ -19,12 +27,16 @@ public static class StrikeAssignmentAnalyzer
         return temp;
     }
 
+    /// <summary>
+    /// מסנן את יחידות התקיפה הזמינות מתוך רשימה נתונה
+    /// </summary>
+    /// <param name="unitsToFilter">רשימת יחידות התקיפה לסינון</param>
+    /// <returns>רשימת יחידות התקיפה הזמינות</returns>
     public static List<StrikeUnit> FilterAvailableUnits(List<StrikeUnit> unitsToFilter)
     {
         var availableUnits = new List<StrikeUnit>();
         foreach (var strikeUnit in unitsToFilter)
         {
-
             if (strikeUnit.IsAvailableForStrike()) // בודק האם היחידה זמינה לתקיפה
             {
                 availableUnits.Add(strikeUnit);
@@ -34,6 +46,12 @@ public static class StrikeAssignmentAnalyzer
         return availableUnits;
     }
 
+    /// <summary>
+    /// מחזיר את יחידת התקיפה הזמינה הראשונה המתאימה לאזור המטרה
+    /// </summary>
+    /// <param name="targetZone">אזור המטרה</param>
+    /// <returns>יחידת התקיפה שנמצאה</returns>
+    /// <exception cref="Exception">נזרק כאשר לא נמצאה יחידת תקיפה מתאימה</exception>
     public static StrikeUnit GetFirstAvailableUnitForStrike(string targetZone)
     {
         List<StrikeUnit> capableUnits = GetUnitsCapableOfTargeting(targetZone);
@@ -44,5 +62,4 @@ public static class StrikeAssignmentAnalyzer
         }
         throw new Exception("No available units for strike");
     }
-
 }
