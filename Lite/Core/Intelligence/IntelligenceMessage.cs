b9a39ad75cad02f3e494715cde882bfb3bb41bd9
eli_shteinman <a0533057932@gmail.com>
2025-06-05 12:21:42 +0300

    Introduce constructor for `IntelligenceMessage` with target, location, and timestamp properties for improved encapsulation.

--- Lite/Core/Intelligence/IntelligenceMessage.cs
+++ Lite/Core/Intelligence/IntelligenceMessage.cs
@@ -1,6 +1,16 @@
+using Lite.Core.Contracts;
+using Lite.Core.Enum;
 namespace Lite.Core.Intelligence;
 
 public class IntelligenceMessage
 {
-
+    public ITerrorist Target { get; private set; }
+    public LocationType LastLocation { get; private set; }
+    public DateTime LastSeen { get; private set; }
+    public IntelligenceMessage(ITerrorist target, LocationType lastLocation, DateTime lastSeen)
+    {
+        Target = target;
+        LastLocation = lastLocation;
+        LastSeen = lastSeen;
+    }
 }
